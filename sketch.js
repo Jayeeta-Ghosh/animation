4e02da861cc69f62d9126f26f76efc6e70a6ae79
Jayeeta-Ghosh <84728726+Jayeeta-Ghosh@users.noreply.github.com>
2021-07-25 12:19:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var backgroundImg;
+
+var boy;
+var snow;
+
+function setup() {
+  createCanvas(800,400);
+  
+  
+  backgroundImg = loadImage("snow1.jpg");
+  boyImg = loadAnimation("boy.jpg");
+  snow = new Snow(200,400,10,10);
+
+}
+
+function draw() {
+  background(backgroundImg);
+  
+  boy = createSprite(300,300);
+  boy.addAnimation("run",boyImg);
+ boy.scale=0.11;
+
+ snow.display();
+ 
+  drawSprites();
+}
